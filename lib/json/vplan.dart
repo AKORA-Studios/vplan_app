41e4e8ece02e616152c8b50c29b9ccab7911c0ae
AKORA <dragoncatm@gmail.com>
2022-03-15 20:57:33 +0100

    Add VPlan class

---
+++ lib/json/vplan.dart
@@ -0,0 +1,155 @@
+// To parse this JSON data, do
+//
+//     final vPlan = vPlanFromJson(jsonString);
+
+import 'dart:convert';
+
+VPlan vPlanFromJson(String str) => VPlan.fromJson(json.decode(str));
+
+String vPlanToJson(VPlan data) => json.encode(data.toJson());
+
+class VPlan {
+  VPlan({
+    required this.type,
+    required this.head,
+    required this.body,
+    required this.info,
+  });
+
+  String type;
+  Head head;
+  List<Body> body;
+  String info;
+
+  factory VPlan.fromJson(Map<String, dynamic> json) => VPlan(
+        type: json["_type"],
+        head: Head.fromJson(json["head"]),
+        body: List<Body>.from(json["body"].map((x) => Body.fromJson(x))),
+        info: json["info"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "_type": type,
+        "head": head.toJson(),
+        "body": List<dynamic>.from(body.map((x) => x.toJson())),
+        "info": info,
+      };
+}
+
+class Body {
+  Body({
+    required this.id,
+    required this.bodyClass,
+    required this.lesson,
+    required this.subject,
+    required this.teacher,
+    required this.room,
+    required this.info,
+    required this.changed,
+  });
+
+  int id;
+  String bodyClass;
+  String lesson;
+  String subject;
+  String teacher;
+  String room;
+  String info;
+  List<ChangedElement> changed;
+
+  factory Body.fromJson(Map<String, dynamic> json) => Body(
+        id: json["_id"],
+        bodyClass: json["class"],
+        lesson: json["lesson"],
+        subject: json["subject"],
+        teacher: json["teacher"],
+        room: json["room"],
+        info: json["info"],
+        changed: List<ChangedElement>.from(
+            json["changed"].map((x) => changedElementValues.map[x])),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "_id": id,
+        "class": bodyClass,
+        "lesson": lesson,
+        "subject": subject,
+        "teacher": teacher,
+        "room": room,
+        "info": info,
+        "changed": List<dynamic>.from(
+            changed.map((x) => changedElementValues.reverse[x])),
+      };
+}
+
+enum ChangedElement { SUBJECT, TEACHER, ROOM }
+
+final changedElementValues = EnumValues({
+  "room": ChangedElement.ROOM,
+  "subject": ChangedElement.SUBJECT,
+  "teacher": ChangedElement.TEACHER
+});
+
+class Head {
+  Head({
+    required this.title,
+    required this.schoolname,
+    required this.created,
+    required this.changed,
+    required this.missing,
+  });
+
+  String title;
+  String schoolname;
+  String created;
+  ChangedClass changed;
+  ChangedClass missing;
+
+  factory Head.fromJson(Map<String, dynamic> json) => Head(
+        title: json["title"],
+        schoolname: json["schoolname"],
+        created: json["created"],
+        changed: ChangedClass.fromJson(json["changed"]),
+        missing: ChangedClass.fromJson(json["missing"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "title": title,
+        "schoolname": schoolname,
+        "created": created,
+        "changed": changed.toJson(),
+        "missing": missing.toJson(),
+      };
+}
+
+class ChangedClass {
+  ChangedClass({
+    required this.classes,
+    required this.teachers,
+  });
+
+  String classes;
+  String teachers;
+
+  factory ChangedClass.fromJson(Map<String, dynamic> json) => ChangedClass(
+        classes: json["classes"],
+        teachers: json["teachers"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "classes": classes,
+        "teachers": teachers,
+      };
+}
+
+class EnumValues<T> {
+  late Map<String, T> map;
+  late Map<T, String> reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    reverseMap ??= map.map((k, v) => MapEntry(v, k));
+    return reverseMap;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'VPlan'),
     );
   }
 }
@@ -52,58 +52,32 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 6,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+            padding: const EdgeInsets.all(10),
+            itemCount: 300,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                margin: const EdgeInsets.only(bottom: 2, top: 2),
+                color: Colors.amber,
+                child: Center(child: Text('$index')),
+              );
+            }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
